eaf80e326d9e0b00a4980e09f81792fd936fba05
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-09-21 20:44:25 +0200

    feat: add to parametrize pytest.param with id of tests

--- app/test_main.py
+++ app/test_main.py
@@ -6,15 +6,36 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
+        pytest.param(
+            0, 0, [0, 0],
+            id="should return 0 when years are 0"
+        ),
+        pytest.param(
+            14, 14, [0, 0],
+            id="should return 0 when years are 14"
+        ),
+        pytest.param(
+            15, 15, [1, 1],
+            id="should return 1 when years are 15"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="should return 1 when years are 23"),
+        pytest.param(
+            24, 24, [2, 2],
+            id="should return 2 when years are 24"
+        ),
+        pytest.param(
+            27, 27, [2, 2],
+            id="should return 2 when years are 27"
+        ),
     ]
 )
-def test_cat_and_dog_age(cat_age, dog_age, result) -> None:             #1
+def test_cat_and_dog_age(
+        cat_age: int,
+        dog_age: int ,
+        result: list[int, int]
+) -> None:
     assert get_human_age(cat_age, dog_age) == result
 
 
