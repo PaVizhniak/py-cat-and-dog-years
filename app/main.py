a106b8405658a62a7624f174ee9f8af99721c93e
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-09-21 20:39:13 +0200

    feat: add validator for less then 0 and grater then 100

--- app/main.py
+++ app/main.py
@@ -1,32 +1,33 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
     result = [0, 0]
 
-    while cat_age >= 15:
-        if cat_age >= 15:
-            result[0] += 1
-            cat_age -= 15
-
-            if cat_age >= 9:
+    if (cat_age >= 0 <= dog_age) and (cat_age <= 100 >= dog_age):
+        while cat_age >= 15:
+            if cat_age >= 15:
                 result[0] += 1
-                cat_age -= 9
+                cat_age -= 15
 
-                if cat_age >= 4:
-                    total_cat_age = cat_age // 4
-                    result[0] += total_cat_age
-                    cat_age -= cat_age
+                if cat_age >= 9:
+                    result[0] += 1
+                    cat_age -= 9
 
-    while dog_age >= 15:
-        if dog_age >= 15:
-            result[1] += 1
-            dog_age -= 15
+                    if cat_age >= 4:
+                        total_cat_age = cat_age // 4
+                        result[0] += total_cat_age
+                        cat_age -= cat_age
 
-            if dog_age >= 9:
+        while dog_age >= 15:
+            if dog_age >= 15:
                 result[1] += 1
-                dog_age -= 9
+                dog_age -= 15
 
-                if dog_age >= 5:
-                    total_dog_age = dog_age // 5
-                    result[1] += total_dog_age
-                    dog_age -= dog_age
+                if dog_age >= 9:
+                    result[1] += 1
+                    dog_age -= 9
+
+                    if dog_age >= 5:
+                        total_dog_age = dog_age // 5
+                        result[1] += total_dog_age
+                        dog_age -= dog_age
 
     return result
