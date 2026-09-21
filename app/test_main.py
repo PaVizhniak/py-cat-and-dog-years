cd238dd320a3141ac4dfec25783762e2fe51843c
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-09-21 19:56:40 +0200

    feat: add tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_0_when_years_equals_0() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_return_0_years_when_years_equals_14() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_2_years_when_years_equals_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_return_3_and_2_when_years_equals_28() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_return_21_and_17_when_years_equals_100() -> None:
+    assert get_human_age(100, 100) == [21, 17]
