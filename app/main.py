abc2756f2629e1076c393e40d2e1ed70842371be
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-09-22 18:55:24 +0200

    address requested changes

--- app/main.py
+++ app/main.py
@@ -1,33 +1,41 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
     result = [0, 0]
 
-    if (cat_age >= 0 <= dog_age) and (cat_age <= 100 >= dog_age):
-        while cat_age >= 15:
-            if cat_age >= 15:
+
+    if type(cat_age) is not int or type(dog_age) is not int:
+        raise TypeError("Values must be integers")
+
+    if not (0 <= cat_age <= 1000) or not (0 <= dog_age <= 1000):
+        return result
+
+    while cat_age >= 15:
+        if cat_age >= 15:
+            result[0] += 1
+            cat_age -= 15
+
+            if cat_age >= 9:
                 result[0] += 1
-                cat_age -= 15
+                cat_age -= 9
 
-                if cat_age >= 9:
-                    result[0] += 1
-                    cat_age -= 9
+                if cat_age >= 4:
+                    total_cat_age = cat_age // 4
+                    result[0] += total_cat_age
+                    cat_age -= cat_age
 
-                    if cat_age >= 4:
-                        total_cat_age = cat_age // 4
-                        result[0] += total_cat_age
-                        cat_age -= cat_age
+    while dog_age >= 15:
+        if dog_age >= 15:
+            result[1] += 1
+            dog_age -= 15
 
-        while dog_age >= 15:
-            if dog_age >= 15:
+            if dog_age >= 9:
                 result[1] += 1
-                dog_age -= 15
+                dog_age -= 9
 
-                if dog_age >= 9:
-                    result[1] += 1
-                    dog_age -= 9
-
-                    if dog_age >= 5:
-                        total_dog_age = dog_age // 5
-                        result[1] += total_dog_age
-                        dog_age -= dog_age
+                if dog_age >= 5:
+                    total_dog_age = dog_age // 5
+                    result[1] += total_dog_age
+                    dog_age -= dog_age
 
     return result
+
+# print(get_human_age("100", 1001))
--- app/test_main.py
+++ app/test_main.py
@@ -29,19 +29,50 @@ from app.main import get_human_age
             27, 27, [2, 2],
             id="should return 2 when years are 27"
         ),
+        pytest.param(
+            28, 28, [3, 2],
+            id="should return 3 and 2 when years equal 28"
+        ),
+        pytest.param(
+            100, 100, [21, 17],
+            id="should return 21 and 17 when years equal 100"
+        ),
+        pytest.param(
+            -1, 1000, [0, 0],
+            id="return 0, 0 when cat_age is less than 0"
+        ),
+        pytest.param(
+            1000, -1, [0, 0],
+            id="return 0, 0 when dog_age is less than 0"
+        ),
+        pytest.param(
+            1001, 1000, [0, 0],
+            id="return 0, 0 when cat_age is greater than 1000"
+        ),
+        pytest.param(
+            1000, 1001, [0, 0],
+            id="return 0, 0 when dog_age is greater than 1000"
+        ),
     ]
 )
 def test_cat_and_dog_age(
         cat_age: int,
         dog_age: int ,
-        result: list[int, int]
+        result: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == result
 
 
-def test_should_return_3_and_2_when_years_equals_28() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_should_return_21_and_17_when_years_equals_100() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("100", 100),
+        (100, "100"),
+        ("100", "100"),
+        (100.1, 100),
+        (100, 100.1),
+    ]
+)
+def test_should_raise_type_error_when_values_are_not_integers( cat_age: int, dog_age: int):
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
